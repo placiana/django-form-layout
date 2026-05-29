09a11ad97e5856aa6542eb84d80eada231a6da9f
Pablo Laciana <placiana@dc.uba.ar>
2026-05-28 23:56:41 -0300

    setup updated

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-form-layout',
     version='0.1.dev',
     author='Joni Bekenstein, Ivan Raskovsky (rasca)',
-    author_email='joni@mindpulse.net',
-    packages=['form_layout',],
+    # find_packages() encontrará form_layout y form_layout.templatetags
+    packages=find_packages(), 
+    include_package_data=True, # Importante para incluir templates HTML si los hay
     license='BSD',
     description='',
     long_description=open('README.txt').read(),
